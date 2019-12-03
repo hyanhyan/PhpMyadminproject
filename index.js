f580b3e06c98a2910f03682870c0ee4c58d82eb4
Zara <zar.harutyunyan.97@mail.ru>
2019-12-03 17:59:56 +0400

    all

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-function myFunction() {
-    if (document.getElementById('general')) {
-
-        if (document.getElementById('general').style.display == 'none') {
-            document.getElementById('general').style.display = 'block';
-            document.getElementById('table').style.display = 'none';
-        }
-        else {
-            document.getElementById('general').style.display = 'none';
-            document.getElementById('table').style.display = 'block';
-        }
+function myFunction(x) {
+    if (x === 1) {
+        document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table1<td>&nbsp;</td></tr></table>";
+    } else if (x === 2) {
+        document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table2<td>&nbsp;</td></tr></table>";
+    } else if (x === 3) {
+        document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table3<td>&nbsp;</td></tr></table>";
+    } else if (x === 4) {
+        document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table4<td>&nbsp;</td></tr></table>";
+    } else {
+        document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table5<td>&nbsp;</td></tr></table>";
     }
 }
+
+/*
+function myFunc(number) {
+    let arr1=["100px","100px","100px"];
+    let arr2=["#cef500","#cbeef3","#e1bbc7"];
+    for (let i = 0,j=0; i < arr1.length,j<arr2.length; i++,j++) {
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute('id', 'square'+i);
+        document.body.appendChild(newDiv);
+        document.getElementById('square'+i).style.width = arr1[i];
+        document.getElementById('square'+i).style.height = arr1[i];
+        document.getElementById('square'+i).style.background = arr2[j];
+    }
+}
+myFunc();
+*/
