5930f8fa30dd4d2f3f1dddaca36431b6a6d2c2a9
Zara <zar.harutyunyan.97@mail.ru>
2019-12-04 16:59:45 +0400

    changess

--- index.js
+++ index.js
@@ -12,18 +12,58 @@ function myFunction(x) {
     }
 }
 
-/*
-function myFunc(number) {
-    let arr1=["100px","100px","100px"];
-    let arr2=["#cef500","#cbeef3","#e1bbc7"];
-    for (let i = 0,j=0; i < arr1.length,j<arr2.length; i++,j++) {
-        let newDiv = document.createElement("div");
-        newDiv.setAttribute('id', 'square'+i);
-        document.body.appendChild(newDiv);
-        document.getElementById('square'+i).style.width = arr1[i];
-        document.getElementById('square'+i).style.height = arr1[i];
-        document.getElementById('square'+i).style.background = arr2[j];
+
+let menus=[
+    {name:"nav-item", img:"img/s_db.png", title:"Databases"
+    },
+    {name:"nav-item", img:"img/b_sql.png", title:"Sql"
+    },
+    {name: "nav-item", img: "img/s_status.png", title: "Status"
+    },
+    {name: "nav-item", img: "img/s_host.png", title: "Users"
+    },
+    {name: "nav-item", img: "img/b_export.png", title: "Export"
+    },
+    {name:"nav-item", img:"img/b_import.png", title:"Import"
+    },
+    {name: "nav-item", img: "img/b_tblops.png", title: "Settings"
     }
+];
+var parent = document.getElementById('navigate');
+for (var i of menus){
+    // console.log(i['name']);
+    var li=document.createElement('li');
+    li.setAttribute('class', i.name);
+    parent.appendChild(li);
+    var img = document.createElement('img');
+    img.src =i.img;
+    img.setAttribute('class','home');
+    img.setAttribute('alt','');
+    img.style.width=("16px");
+    img.style.height=("16px");
+    li.appendChild(img);
+    var ah=document.createElement('a');
+    ah.setAttribute('class', 'nav-link btn btn');
+    ah.setAttribute('href', '#');
+    ah.setAttribute('role', 'button');
+    ah.innerHTML=i.title;
+    li.appendChild(ah);
 }
-myFunc();
-*/
+var arr=[
+    {img:"img/b_plus.png", href:"#multiCollapseExample1", name:"toggle 1 element"
+    },
+    {img:"img/b_plus.png", href:"#multiCollapseExample2", name:"toggle 2 element"
+    },
+    {img:"img/b_plus.png", href:"#multiCollapseExample3", name:"toggle 3 element"
+    },
+    {img:"img/b_plus.png", href:"#multiCollapseExample4", name:"toggle 4 element"
+    },
+    {img:"img/b_plus.png", href:"#multiCollapseExample5", name:"toggle 5 element"
+    },
+    {img:"img/b_plus.png", href:"#multiCollapseExample6", name:"toggle 6 element"
+    }
+];
+var newDiv=document.getElementById('all');
+for (var i of arr) {
+
+}
