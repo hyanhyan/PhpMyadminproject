5a989ddaa5a55c6b203f249095855f93867d5bc9
hyanhyan <zar.harutyunyan.97@mail.ru>
2019-12-05 21:28:16 +0300

    jschanges

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ let menus=[
 ];
 var parent = document.getElementById('navigate');
 for (var i of menus){
-    // console.log(i['name']);
+     console.log(i['name']);
     var li=document.createElement('li');
     li.setAttribute('class', i.name);
     parent.appendChild(li);
@@ -63,60 +63,94 @@ for (var i of menus){
     li.appendChild(ah);
 }
 var arr=[
-    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample1",tog:"row",child:"col",classname:'div1',btn:"btn btn",href:"#multiCollapseExample1",onclick:"myFunction(1)" ,name:"toggle 1 element",data:"collapse",class:"plus",controls:"multiCollapseExample1",role:"button",src:"multiCollapseExample1",expanded:"true"
-    },
-    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample11",tog:"row",child:"col",classname:'div1', btn:"btn btn",href:"#multiCollapseExample11", name:"toggle 1.1 element",data:"collapse",class:"plus",controls:"multiCollapseExample11",role:"button",src:"multiCollapseExample1.1",expanded:"true"
-    },
-    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample2",tog:"row",child:"col",classname:'div1',btn:"btn btn", href:"#multiCollapseExample2",onclick:"myFunction(2)",name:"toggle 2 element",data:"collapse",class:"plus",controls:"multiCollapseExample2",role:"button",src:"multiCollapseExample2",expanded:"true"
-    },
-    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample22",tog:"row",child:"col",classname:'div1',btn:"btn btn", href:"#multiCollapseExample22", name:"toggle 2.2 element",data:"collapse",class:"plus",controls:"multiCollapseExample22",role:"button",src:"multiCollapseExample2.2",expanded:"true"
-    },
-    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample3",tog:"row",child:"col",classname:'div1',btn:"btn btn", href:"#multiCollapseExample3",onclick:"myFunction(3)", name:"toggle 3 element",data:"collapse",class:"plus",controls:"multiCollapseExample3",role:"button",src:"multiCollapseExample3",expanded:"true"
-    },
-    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample33",tog:"row",child:"col", classname:'div1',btn:"btn btn",href:"#multiCollapseExample33", name:"toggle 3.3 element",data:"collapse",class:"plus",controls:"multiCollapseExample33",role:"button",src:"multiCollapseExample3.1",expanded:"true"
-    },
-];
-/*$('<div>',{class:i.classname,css:{width:100,height:100}}).appendTo('#all');*/
-console.log(arr[i]);
-    var all=document.getElementById('all');
-    $(document).ready(function(){
-    for(var i of arr) {
-        var div1=(document.createElement('div'));
-        div1.setAttribute('class',i.classname);
-        div1.style.width=("300px");
-        div1.style.height=("100px");
-        $(div1).appendTo(all);
-        var img=(document.createElement('img'));
-        img.src=i.img;
-        img.setAttribute('href',i.href);
-        img.setAttribute('alt','');
-        img.setAttribute('data-toggle',i.data);
-        img.setAttribute('class',i.class);
-        img.setAttribute('aria-controls',i.controls);
-        img.style.width=("16px");
-        img.style.height=("16px");
-        $(img).appendTo(div1);
-        var ah=document.createElement('a');
-        ah.setAttribute('class',i.btn);
-        ah.setAttribute('role',i.role);
-        ah.setAttribute('data-toggle',i.data);
-        ah.setAttribute('aria-controls',i.src);
-        ah.setAttribute('aria-expanded',i.expanded);
-        ah.setAttribute('href',i.href);
-        ah.setAttribute('onclick',i.onclick);
-        $(ah).text(i.name);
-        $(ah).appendTo(div1);
-        var row=(document.createElement('div'));
-        row.setAttribute('class',i.tog);
-        $(row).appendTo(all);
-        var col=(document.createElement('div'));
-        col.setAttribute('class',i.child);
-        $(col).appendTo(row);
-        var inner=(document.createElement('div'));
-        inner.setAttribute('id',i.id);
-        inner.setAttribute('class',i.inner);
-        $(inner).appendTo(col);
-    }
-    });
+     {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample1",tog:"row",child:"col",btn:"btn btn",href:"#multiCollapseExample1",onclick:"myFunction(1)" ,name:"Toggle 1 element",data:"collapse",class:"plus",controls:"multiCollapseExample1",role:"button",src:"multiCollapseExample1",expanded:"true"
+     },
+     {img1:"img/b_plus.png",secondData:"collapse1",type:"button",thirdName:"Toggle 1.1.2",thirdClass:"collapse",tog:"row",child:"col",classname:"btn btn link",secondId:"multiCollapseExample11",thirdId:"demo", btn:"btn btn",data:"collapse",class:"plus",role:"button",expanded:"true",secondHref:"#multiCollapseExample11",secondControls:"multiCollapseExample11",thirdHref:"multiCollapseExample11",secondName:"Toggle 1.1 element",target:"#demo"
+     },
+     {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample2",tog:"row",child:"col",btn:"btn btn", href:"#multiCollapseExample2",onclick:"myFunction(2)",name:"Toggle 2 element",data:"collapse",class:"plus",controls:"multiCollapseExample2",role:"button",src:"multiCollapseExample2",expanded:"true"
+     },
+     {img1:"img/b_plus.png",secondData:"collapse2",type:"button",thirdName:"Toggle 2.1.2",thirdClass:"collapse1",tog:"row",child:"col",classname:"btn btn link",secondId:"multiCollapseExample22",thirdId:"demo1",btn:"btn btn", data:"collapse",class:"plus",role:"button",expanded:"true",secondHref:"#multiCollapseExample22",secondControls:"multiCollapseExample22",thirdHref:"multiCollapseExample22",secondName:"Toggle 2.1 element",target:"#demo1"
+     },
+     {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample3",tog:"row",child:"col",btn:"btn btn", href:"#multiCollapseExample3",onclick:"myFunction(3)", name:"Toggle 3 element",data:"collapse",class:"plus",controls:"multiCollapseExample3",role:"button",src:"multiCollapseExample3",expanded:"true"
+     },
+     {img1:"img/b_plus.png",secondData:"collapse3",type:"button",thirdName:"Toggle 3.1.2",thirdClass:"collapse3",tog:"row",child:"col", classname:"btn btn link",secondId:"multiCollapseExample33",thirdId:"demo3",btn:"btn btn", data:"collapse",class:"plus",role:"button",expanded:"true",secondHref:"#multiCollapseExample33",secondControls:"multiCollapseExample33",thirdHref:"multiCollapseExample33",secondName:"Toggle 3.1 element",target:"#demo3"
+     },
+ ];
+ /*$('<div>',{class:i.classname,css:{width:100,height:100}}).appendTo('#all');*/
+ console.log(arr[i]);
+     var all=document.getElementById('all');
+     $(document).ready(function(){
+     for(var i of arr) {
+         var p=(document.createElement('div'));
+         p.setAttribute('class',i.classname);
+         $(p).appendTo(all);
+         var img=(document.createElement('img'));
+         img.src=i.img;
+         img.setAttribute('href',i.href);
+         img.setAttribute('alt','');
+         img.setAttribute('data-toggle',i.data);
+         img.setAttribute('class',i.class);
+         img.setAttribute('aria-controls',i.controls);
+         img.style.width=("16px");
+         img.style.height=("16px");
+         $(img).appendTo(p);
+         var ah=document.createElement('a');
+         ah.setAttribute('class',i.btn);
+         ah.setAttribute('role',i.role);
+         ah.setAttribute('data-toggle',i.data);
+         ah.setAttribute('aria-controls',i.src);
+         ah.setAttribute('aria-expanded',i.expanded);
+         ah.setAttribute('href',i.href);
+         ah.setAttribute('onclick',i.onclick);
+         $(ah).text(i.name);
+         $(ah).appendTo(p);
+         var row=(document.createElement('div'));
+         row.setAttribute('class',i.tog);
+         $(row).appendTo(all);
+         var col=(document.createElement('div'));
+         col.setAttribute('class',i.child);
+         $(col).appendTo(row);
+         var inner1=(document.createElement('div'));
+         inner1.setAttribute('id',i.id);
+         inner1.setAttribute('class',i.inner);
+         $(inner1).appendTo(col);
+         var p1=(document.createElement('p'));
+         $(p1).appendTo(inner1);
+         var img11=(document.createElement('img'));
+         img11.src=i.img;
+         img11.setAttribute('href',i.secondHref);
+         img11.setAttribute('alt','');
+         img11.setAttribute('data-toggle',i.data);
+         img11.setAttribute('class',i.class);
+         img11.setAttribute('aria-controls',i.secondControls);
+         img11.style.width=("16px");
+         img11.style.height=("16px");
+         $(img11).appendTo(p1);
+         var ah1=document.createElement('a');
+         ah1.setAttribute('class',i.btn);
+         ah1.setAttribute('role',i.role);
+         ah1.setAttribute('data-toggle',i.data);
+         ah1.setAttribute('aria-controls',i.thirdHref);
+         ah1.setAttribute('aria-expanded',i.expanded);
+         ah1.setAttribute('href',i.thirdHref);
+         $(ah1).text(i.secondName);
+         $(ah1).appendTo(p1);
+         var inner2=(document.createElement('div'));
+         inner2.setAttribute('id',i.secondId);
+         inner2.setAttribute('class',i.inner);
+         $(inner2).appendTo(inner1);
+         var button=(document.createElement('button'));
+         button.setAttribute('type',i.type);
+         button.setAttribute('class',i.classname);
+         button.setAttribute('data-toggle',i.secondData);
+         button.setAttribute('data-target',i.target);
+         $(button).text(i.thirdName);
+         $(button).appendTo(inner2);
+         var demo=(document.createElement('div'));
+         demo.setAttribute('id',i.thirdId);
+         demo.setAttribute('class',i.thirdClass);
+         $(demo).appendTo(inner2);
+     }
+     });
 
 
