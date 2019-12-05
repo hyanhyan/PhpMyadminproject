bbbd0c3a753c235e8ccca93f07526259476e43fa
Zara <zar.harutyunyan.97@mail.ru>
2019-12-05 17:32:08 +0400

    change

--- index.js
+++ index.js
@@ -3,14 +3,27 @@ function myFunction(x) {
         document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table1<td>&nbsp;</td></tr></table>";
     } else if (x === 2) {
         document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table2<td>&nbsp;</td></tr></table>";
-    } else if (x === 3) {
-        document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table3<td>&nbsp;</td></tr></table>";
-    } else if (x === 4) {
-        document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table4<td>&nbsp;</td></tr></table>";
     } else {
-        document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table5<td>&nbsp;</td></tr></table>";
+        document.getElementById('general').innerHTML = "<table width='100%' border='1'><tr><td>&nbsp;</td>Table3<td>&nbsp;</td></tr></table>";
+    }
+
+}
+/*
+function togDropdown() {
+    if (document.getElementsByClassName('plus')) {
+        if (document.getElementById('plus').style.display == 'none') {
+            document.getElementById('plus').style.display = 'block';
+            document.getElementById('minus').style.display = 'none';
+        } else {
+            document.getElementById('plus').style.display = 'none';
+            document.getElementById('minus').style.display = 'block';
+        }
     }
 }
+togDropdown();
+*/
+
+
 
 
 let menus=[
@@ -50,20 +63,60 @@ for (var i of menus){
     li.appendChild(ah);
 }
 var arr=[
-    {img:"img/b_plus.png", href:"#multiCollapseExample1", name:"toggle 1 element"
+    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample1",tog:"row",child:"col",classname:'div1',btn:"btn btn",href:"#multiCollapseExample1",onclick:"myFunction(1)" ,name:"toggle 1 element",data:"collapse",class:"plus",controls:"multiCollapseExample1",role:"button",src:"multiCollapseExample1",expanded:"true"
     },
-    {img:"img/b_plus.png", href:"#multiCollapseExample2", name:"toggle 2 element"
+    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample11",tog:"row",child:"col",classname:'div1', btn:"btn btn",href:"#multiCollapseExample11", name:"toggle 1.1 element",data:"collapse",class:"plus",controls:"multiCollapseExample11",role:"button",src:"multiCollapseExample1.1",expanded:"true"
     },
-    {img:"img/b_plus.png", href:"#multiCollapseExample3", name:"toggle 3 element"
+    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample2",tog:"row",child:"col",classname:'div1',btn:"btn btn", href:"#multiCollapseExample2",onclick:"myFunction(2)",name:"toggle 2 element",data:"collapse",class:"plus",controls:"multiCollapseExample2",role:"button",src:"multiCollapseExample2",expanded:"true"
     },
-    {img:"img/b_plus.png", href:"#multiCollapseExample4", name:"toggle 4 element"
+    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample22",tog:"row",child:"col",classname:'div1',btn:"btn btn", href:"#multiCollapseExample22", name:"toggle 2.2 element",data:"collapse",class:"plus",controls:"multiCollapseExample22",role:"button",src:"multiCollapseExample2.2",expanded:"true"
     },
-    {img:"img/b_plus.png", href:"#multiCollapseExample5", name:"toggle 5 element"
+    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample3",tog:"row",child:"col",classname:'div1',btn:"btn btn", href:"#multiCollapseExample3",onclick:"myFunction(3)", name:"toggle 3 element",data:"collapse",class:"plus",controls:"multiCollapseExample3",role:"button",src:"multiCollapseExample3",expanded:"true"
     },
-    {img:"img/b_plus.png", href:"#multiCollapseExample6", name:"toggle 6 element"
-    }
+    {img:"img/b_plus.png",inner:"collapse multi-collapse inner",id:"multiCollapseExample33",tog:"row",child:"col", classname:'div1',btn:"btn btn",href:"#multiCollapseExample33", name:"toggle 3.3 element",data:"collapse",class:"plus",controls:"multiCollapseExample33",role:"button",src:"multiCollapseExample3.1",expanded:"true"
+    },
 ];
-var newDiv=document.getElementById('all');
-for (var i of arr) {
+/*$('<div>',{class:i.classname,css:{width:100,height:100}}).appendTo('#all');*/
+console.log(arr[i]);
+    var all=document.getElementById('all');
+    $(document).ready(function(){
+    for(var i of arr) {
+        var div1=(document.createElement('div'));
+        div1.setAttribute('class',i.classname);
+        div1.style.width=("300px");
+        div1.style.height=("100px");
+        $(div1).appendTo(all);
+        var img=(document.createElement('img'));
+        img.src=i.img;
+        img.setAttribute('href',i.href);
+        img.setAttribute('alt','');
+        img.setAttribute('data-toggle',i.data);
+        img.setAttribute('class',i.class);
+        img.setAttribute('aria-controls',i.controls);
+        img.style.width=("16px");
+        img.style.height=("16px");
+        $(img).appendTo(div1);
+        var ah=document.createElement('a');
+        ah.setAttribute('class',i.btn);
+        ah.setAttribute('role',i.role);
+        ah.setAttribute('data-toggle',i.data);
+        ah.setAttribute('aria-controls',i.src);
+        ah.setAttribute('aria-expanded',i.expanded);
+        ah.setAttribute('href',i.href);
+        ah.setAttribute('onclick',i.onclick);
+        $(ah).text(i.name);
+        $(ah).appendTo(div1);
+        var row=(document.createElement('div'));
+        row.setAttribute('class',i.tog);
+        $(row).appendTo(all);
+        var col=(document.createElement('div'));
+        col.setAttribute('class',i.child);
+        $(col).appendTo(row);
+        var inner=(document.createElement('div'));
+        inner.setAttribute('id',i.id);
+        inner.setAttribute('class',i.inner);
+        $(inner).appendTo(col);
+    }
+    });
 
-}
+
